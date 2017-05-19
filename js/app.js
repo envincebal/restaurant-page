b3aed811d9b32edadbc5e33456437edc68360cf8
Vincent Yan <vincentyan1@gmail.com>
2017-05-19 15:43:56 -0700

    Switched from proxy to JSONP call. Added info on current weather condition

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
+
+	var geolocation = "https://freegeoip.net/json/";
 	
-	$.getJSON("https://freegeoip.net/json/", function(data){
+	$.ajax({
+		url: geolocation,
+		dataType: "jsonp"
+	}).done(function(data){
+
 		var location = $(".location");
+		var temperature = $(".temperature");
+		var condition = $(".condition");
+		var geoURL = "https://api.darksky.net/forecast/8b01861d3b06ab86ba285ef08d52c88d/";
+		var lat = data.latitude;
+		var long = data.longitude;
+		var yourLocation = geoURL + lat + "," + long;
+
 		location.text(data.city + ", " + data.region_name);
 
+		$.ajax({
+			url: yourLocation,
+			dataType: "jsonp"
+		}).done(function(data2){
+			var fahrenheit = data2.currently.temperature;
+			var currentCondition = data2.currently.summary;
+
+			temperature.html(parseInt(fahrenheit) + "&deg" + "F");
+			condition.html(currentCondition);
+		});
 	});
 });
-
