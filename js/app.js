663da6de63fd8f2bd6e3a1ed458f69329ff5c036
Vincent Yan <vincentyan1@gmail.com>
2017-05-18 22:15:47 -0700

    Added getJSON method to retrieve city and state data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-(function() {
+$(document).ready(function(){
 	
+	$.getJSON("https://freegeoip.net/json/", function(data){
+		var location = $(".location");
+		location.text(data.city + ", " + data.region_name);
 
-})
+	});
+});
+
