c271e339b9172ac22535881c70c4ba1ece8f9cc7
Vincent Yan <vincentyan1@gmail.com>
2017-05-19 23:02:09 -0700

    Added toggle button functionality

--- js/app.js
+++ js/app.js
@@ -9,13 +9,16 @@ $(document).ready(function(){
 
 		var location = $(".location");
 		var temperature = $(".temperature");
+		var icon = $(".icon");
 		var condition = $(".condition");
+		var button = $(".change");
 		var geoURL = "https://api.darksky.net/forecast/8b01861d3b06ab86ba285ef08d52c88d/";
 		var lat = data.latitude;
 		var long = data.longitude;
 		var yourLocation = geoURL + lat + "," + long;
+		var toggle = true;
 
-		location.text(data.city + ", " + data.region_name);
+		location.html(data.city + ", " + data.region_name);
 
 		$.ajax({
 			url: yourLocation,
@@ -23,9 +26,22 @@ $(document).ready(function(){
 		}).done(function(data2){
 			var fahrenheit = data2.currently.temperature;
 			var currentCondition = data2.currently.summary;
-
+			var weatherIcon = data2.currently.icon;
+			
 			temperature.html(parseInt(fahrenheit) + "&deg" + "F");
 			condition.html(currentCondition);
+
+			button.click(function(){
+				if (toggle) {
+					fahrenheit = (fahrenheit - 32) * 5/9;
+					temperature.html(parseInt(fahrenheit) + "&deg" + "C");
+					toggle = false;
+				} else {
+					fahrenheit = fahrenheit * 1.8 + 32;
+					temperature.html(parseInt(fahrenheit) + "&deg" + "F");
+					toggle = true;
+				}
+			});
 		});
 	});
 });
