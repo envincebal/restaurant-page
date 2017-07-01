25330ba99fa8923499a86b4f62111caee4d6292b
Vincent Yan <vincentyan1@gmail.com>
2017-07-01 01:02:55 -0700

    Added additional weather data

--- js/app.js
+++ js/app.js
@@ -1,77 +1,63 @@
 $.getJSON("https://freegeoip.net/json/", function(data){
 	
-	var location = $(".location");
-	var condition = $(".condition");
-	var temperature = $(".temperature");
-	var change = $(".change");
-
-	var geoURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8b01861d3b06ab86ba285ef08d52c88d/" + data.latitude + "," + data.longitude;;
-	var toggle = true;
-
+	var geoURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8b01861d3b06ab86ba285ef08d52c88d/" + data.latitude + "," + data.longitude;
+  var change = $(".change");
+	
 	$.getJSON(geoURL, function(data2){
+		
     var today = new Date();
-    var date = (today.getMonth()+1)+ '/' + today.getDate()  + '/' + today.getFullYear();
+    var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
     var time = today.getHours() + ":" + today.getMinutes();
-		var fahrenheit = data2.currently.temperature;
-		var currentCondition = data2.currently.summary;
-		var weatherIcon = data2.currently.icon;
-		var skycons = new Skycons({"color":"#fff"});
+    $(".current-date").text(date);
     
-    $(".current-date").html(date);
-    location.text(data.city + ", " + data.region_name);
-    condition.html(currentCondition);
-		skycons.add("icon", weatherIcon);
+    $(".location").text(data.city + ", " + data.region_name);
+    
+    $(".condition").text(data2.currently.summary);
+    
+    var skycons = new Skycons({"color":"#fff"});
+		skycons.add("icon", data2.currently.icon);
 		skycons.play();
-		temperature.html(parseInt(fahrenheit) + "&deg") + change.html("F");
-
+    
+    var fahrenheit = data2.currently.temperature;
+    var temperature = $(".temperature");
+		temperature.text(parseInt(fahrenheit) + "°") + change.text("F");
+    var toggle = true;
+    
 		$(".change").click(function(){
 			if (toggle) {
 				fahrenheit = (fahrenheit - 32) * 5/9;
-				temperature.html(parseInt(fahrenheit) + "&deg") + change.html("C");
+				temperature.text(parseInt(fahrenheit) + "°") + change.text("C");
 				toggle = false;
 			} else {
 				fahrenheit = fahrenheit * 1.8 + 32;
-				temperature.html(parseInt(fahrenheit) + "&deg") + change.html("F");
+				temperature.text(parseInt(fahrenheit) + "°") + change.text("F");
 				toggle = true;
 			}
 		});
+    
+    $(".rain-chance").text( data2.currently.precipProbability + " %");
+    $(".humidity").text(data2.currently.humidity + " %");
+    $(".wind-speed").text(data2.currently.windSpeed + " mph");
+    $(".sunrise").text(timeConverter(data2.daily.data[0].sunriseTime));
+    $(".sunset").text(timeConverter(data2.daily.data[0].sunsetTime));
 	});
 });
 
-function setImage(param) {
-	switch(param) {
-		case "clear-day": 
-			return "img/clear-day.png";
-			break;
-		case "clear-night": 
-			return "img/clear-night.png";
-			break;
-		case "rain": 
-			return "img/rain.png";
-			break;
-		case "snow": 
-			return "img/snow.png";
-			break;
-		case "sleet": 
-			return "img/sleet.png";
-			break;
-		case "wind": 
-			return "img/wind.png";
-			break;
-		case "fog": 
-			return "img/fog.png";
-			break;
-		case "cloudy":
-			return "img/cloudy.png";
-			break;
-		case "partly-cloudy-day":
-			return "img/partly-cloudy-day.png";
-			break;
-		case "partly-cloudy-night":
-			return "img/partly-cloudy-night.png";	
-			break;
-	}
+function timeConverter(unix){
+  var date2 = new Date(unix * 1000);
+  var hours = date2.getHours();
+  var minutes = ("0" + date2.getMinutes()).slice(-2);
+  
+  if (hours >= 12) {
+    hours = hours - 12;
+    minutes = minutes + " pm";
+  }else{
+    hours = hours;
+    minutes = minutes + " am";
+  }
+  var formatTime = hours + ":" + minutes;
+  
+  return formatTime;
 }
-
 
 	
